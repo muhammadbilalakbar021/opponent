1ee0e0ffc0ec0de8cad1b7b15c12cb2239cf29b0
Abdur Rehman <abdur@whydonate.nl>
2022-03-06 03:19:03 +0500

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,13 @@
-import { app, BrowserWindow, screen } from 'electron';
-import * as path from 'path';
-import * as fs from 'fs';
-import * as url from 'url';
+import { app, BrowserWindow, screen } from "electron";
+import * as path from "path";
+import * as fs from "fs";
+import * as url from "url";
 
 let win: BrowserWindow = null;
 const args = process.argv.slice(1),
-  serve = args.some(val => val === '--serve');
+  serve = args.some((val) => val === "--serve");
 
 function createWindow(): BrowserWindow {
-
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
@@ -16,40 +15,41 @@ function createWindow(): BrowserWindow {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
+    width: 700,
     height: size.height,
     webPreferences: {
       nodeIntegration: true,
-      allowRunningInsecureContent: (serve) ? true : false,
-      contextIsolation: false,  // false if you want to run e2e test with Spectron
+      allowRunningInsecureContent: serve ? true : false,
+      contextIsolation: false, // false if you want to run e2e test with Spectron
     },
   });
 
-
   if (serve) {
     win.webContents.openDevTools();
-    require('electron-reload')(__dirname, {
-      electron: require(path.join(__dirname, '/../node_modules/electron'))
+    require("electron-reload")(__dirname, {
+      electron: require(path.join(__dirname, "/../node_modules/electron")),
     });
-    win.loadURL('http://localhost:4200');
+    win.loadURL("http://localhost:4200");
   } else {
     // Path when running electron executable
-    let pathIndex = './index.html';
+    let pathIndex = "./index.html";
 
-    if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
-       // Path when running electron in local folder
-      pathIndex = '../dist/index.html';
+    if (fs.existsSync(path.join(__dirname, "../dist/index.html"))) {
+      // Path when running electron in local folder
+      pathIndex = "../dist/index.html";
     }
 
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, pathIndex),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, pathIndex),
+        protocol: "file:",
+        slashes: true,
+      })
+    );
   }
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -64,25 +64,24 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.on("ready", () => setTimeout(createWindow, 400));
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
+  app.on("window-all-closed", () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
